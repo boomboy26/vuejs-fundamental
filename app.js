836b8882ab95fb8186b60c14be202b0703b77764
Eakkawat Louhawattanavanich <eakkawatlouhawattanavanich@MacBook-Pro-khxng-Eakkawat.local>
2021-07-02 22:12:56 +0700

    Study interporation

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const app = Vue.createApp({
-  template: '<h1>Hello World</h1>',
+  // template: '<h1>Hello {{user}}</h1>',
+  data() {
+    return { nation: 'USO', year: '2010', population: 3222112 };
+  },
+  methods: {
+    getNation() {
+      console.log(this.nation);
+    },
+  },
 });
+
+/*
+data
+method 
+image 
+display data
+*/
 
 app.mount('#app');
